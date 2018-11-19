7ebf38dc2d0b95d417f250012ba49de21436ecf7
Mohammad Parvej <mohammadparvej9094@gmail.com>
2018-11-19 14:39:46 +0600

    Update welcome.blade.php
    change the project name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Payroll
                 </div>
 
                 <div class="links">
